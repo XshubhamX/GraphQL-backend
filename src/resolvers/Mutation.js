3314659452d8aef353c2a92f387f84c56684bd01
ShubhamBhardwaj <shubhambhardwajfdb@gmail.com>
2021-02-13 01:01:20 +0530

    pro project structure implemented

---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,99 @@
+import { v4 as uuid4 } from "uuid";
+
+const Mutation = {
+  createUser(parent, args, { db }, info) {
+    const emailTaken = db.users.find((x) => x.email === args.data.email);
+    if (emailTaken) {
+      throw new Error("Email Already registered");
+    }
+
+    const user = {
+      id: uuid4(),
+      ...args.data,
+    };
+    db.users.push(user);
+
+    return user;
+  },
+  deleteUser(parent, args, { db }, info) {
+    const userIndex = db.users.findIndex((user) => user.id === args.id);
+
+    if (userIndex === -1) {
+      throw new Error("User not found");
+    }
+
+    const deletedUsers = db.users.splice(userIndex, 1);
+
+    db.posts = db.posts.filter((post) => {
+      const match = post.author === args.id;
+
+      if (match) {
+        db.comments = db.comments.filter((comment) => comment.post !== post.id);
+      }
+
+      return !match;
+    });
+    db.comments = db.comments.filter((comment) => comment.author !== args.id);
+
+    return deletedUsers[0];
+  },
+  createPost(parent, args, { db }, info) {
+    const userExists = db.users.some((x) => x.id === args.data.author);
+
+    if (!userExists) {
+      throw new Error("User Not found");
+    }
+
+    const newPost = {
+      id: uuid4(),
+      ...args.data,
+    };
+
+    db.posts.push(newPost);
+
+    return newPost;
+  },
+  deletePost(parent, args, { db }, info) {
+    const postIndex = db.posts.findIndex((x) => x.id === args.id);
+
+    if (postIndex === -1) {
+      throw new Error("Invalid Post");
+    }
+
+    const deletedPost = db.posts.splice(postIndex, 1);
+
+    db.comments = db.comments.filter((x) => x.post !== args.id);
+
+    return deletedPost[0];
+  },
+  createComment(parent, args, { db }, info) {
+    const validComment =
+      db.users.some((x) => x.id === args.data.author) &&
+      db.posts.some((x) => x.id === args.data.post && x.published);
+
+    if (!validComment) {
+      throw new Error("Invalid Comment");
+    }
+
+    const newComment = {
+      id: uuid4(),
+      ...args.data,
+    };
+
+    db.comments.push(newComment);
+
+    return newComment;
+  },
+  deleteComment(parent, args, { db }, info) {
+    const commentIndex = db.posts.findIndex((x) => x.id === args.id);
+
+    if (commentIndex === -1) {
+      throw new Error("Invalid Comment");
+    }
+    const deletedComment = db.comments.splice(commentIndex, 1);
+
+    return deletedComment;
+  },
+};
+
+export { Mutation as default };
